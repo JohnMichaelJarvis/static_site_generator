6d8e1519ee83608dcfc63d60bcc51f09892fe13f
John Michael Jarvis <John_Michael_Jarvis@protonmail.com>
2026-01-22 16:35:42 +0000

    feat: Refactor main script and add TextNode class for text representation chore: Remove unused index.html and styles.css files

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+#!/usr/bin/env python3
+
+from textnode import TextNode
+
+
+def main():
+    test_node = TextNode(text="**Test**", text_type="bold", url="https://test.com")  # type: ignore
+
+    print(test_node)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        """Compare self to another TextNode"""
+
+        compare_text: bool = self.text == other.text
+        compare_text_type: bool = self.text_type == other.text_type
+        compare_url: bool = self.url == other.url
+
+        if compare_text and compare_text_type and compare_url:
+            return True
+
+        return False
+
+    def __repr__(self):
+        """Return a string representation of the TextNode object."""
+        return f"TextNode{self.text, self.text_type, self.url}"
